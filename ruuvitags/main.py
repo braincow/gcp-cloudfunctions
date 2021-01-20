fa7c76d08acce59b6eb743bb010ed53c42793acf
Antti Peltonen <antti.peltonen@iki.fi>
2021-01-20 19:59:55 +0200

    REST api for listing known tags

---
+++ ruuvitags/main.py
@@ -0,0 +1,31 @@
+def ruuvitags(request):
+    from google.cloud import bigquery
+    from flask import jsonify
+    import os
+
+    # check if debug is enabled or not
+    debug_enabled = False
+    if os.environ.get("DEBUG", "false") != "false":
+        debug_enabled = True
+
+    if debug_enabled:
+        print(request)
+
+    # read all known tags from database
+    table_id = os.environ.get("TABLE_ID")
+    if not table_id or table_id == "":
+        raise ValueError("TABLE_ID environment variable cannot be empty")
+
+    bq = bigquery.Client()
+
+    tags_query = "SELECT * FROM {}".format(table_id)
+
+    query_job = bq.query(tags_query)
+    records = [dict(row) for row in query_job]
+
+    if debug_enabled:
+        print(records)
+
+    return jsonify(records)
+
+# eof
